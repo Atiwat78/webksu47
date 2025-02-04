aa815675b3563458e19857b1b173331652ee1a46
pannawit.kh <pannawit.kh@ksu.ac.th>
2025-02-05 00:53:40 +0700

    Added wsgi.py for gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
-from waitress import serve  # หรือ gunicorn ถ้าใช้
-from your_flask_app import app
+from app import app
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000)
+    app.run()

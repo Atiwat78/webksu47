ee7ad4d5c325b6eecd885bb1d638e387938fb2c6
pannawit.kh <pannawit.kh@ksu.ac.th>
2025-02-05 01:42:44 +0700

    Fix app.py and wsgi.py conflict

--- app.py
+++ app.py
@@ -169,7 +169,6 @@ def show_users():
 @app.route('/index')
 def index():
     return render_template('index.html')
-return render_template('index.html')
 
 from flask import Flask, render_template
 
